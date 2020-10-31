a77bc0f2072a5eb315c1bbd2996e42c565e2b8f5
Yash Sahijwani <60649571+Terabyte17@users.noreply.github.com>
2020-10-31 11:49:48 +0530

    base arena added.
    randomization and camera to be added

---
+++ Arena_Test.py
@@ -0,0 +1,6 @@
+import gym
+import vision_arena
+
+if __name__=="__main__":
+    gym.make("vision_arena-v0")
+    time.sleep(100)
--- vision-arena/vision_arena/envs/vision2arena.py
+++ vision-arena/vision_arena/envs/vision2arena.py
@@ -4,7 +4,7 @@ from gym.utils import seeding
 import pybullet as p
 import pybullet_data
 import cv2
-from husky_move import move
+import numpy as np
 
 class VisionArena(gym.Env):
   metadata = {'render.modes': ['human']}
@@ -13,12 +13,15 @@ class VisionArena(gym.Env):
     p.connect(p.GUI)
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
     p.setGravity(0,0,-10)
+    p.loadURDF('rsc/plane.urdf',[0,0,-0.1], useFixedBase=1)
+    p.configureDebugVisualizer(p.COV_ENABLE_WIREFRAME, 0)
+    p.configureDebugVisualizer(p.COV_ENABLE_SHADOWS, 0)
     
     self.load_arena()
-    self.husky = p.loadURDF('husky/husky.urdf',[0,0,0],p.getQuaternionFromEuler([0,0,0]))
+    self.husky = p.loadURDF('husky/husky.urdf',[0,0,0.1],p.getQuaternionFromEuler([0,0,0]))
 
   def move_husky(self, leftFrontWheel, rightFrontWheel, leftRearWheel, RighRearWheel):
-    move(self.husky, leftFrontWheel, rightFrontWheel, leftRearWheel, RighRearWheel)
+    self.move(self.husky, leftFrontWheel, rightFrontWheel, leftRearWheel, RighRearWheel)
 
   def reset(self):
     p.resetSimulation()
@@ -31,33 +34,60 @@ class VisionArena(gym.Env):
       for i in range(9):
           for j in range(9):
               if (i==0 or i==8) and j!=4:
-                  pass
+                 p.loadURDF('rsc/base plate/base plate white.urdf', [3-i*0.75,3-j*0.75,0], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
+                 p.loadURDF('rsc/square/square yellow.urdf', [3-i*0.75,3-j*0.75,0.03], p.getQuaternionFromEuler([0,0,np.pi]), useFixedBase=1)
               #normal squares
               elif (i==1 or i==7) and (j==0 or j==8):
-                  pass
+                 p.loadURDF('rsc/base plate/base plate white.urdf', [3-i*0.75,3-j*0.75,0], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
+                 p.loadURDF('rsc/triangle/triangle red.urdf', [3-i*0.75,3-j*0.75,0.03], p.getQuaternionFromEuler([0,0,-np.pi/4]), useFixedBase=1)
               #normal squares
               elif (i==2 or i==6) and (j!=1 and j!=7 and j!=4):
-                  pass
+                 p.loadURDF('rsc/base plate/base plate white.urdf', [3-i*0.75,3-j*0.75,0], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
+                 p.loadURDF('rsc/circle/circle yellow.urdf', [3-i*0.75,3-j*0.75,0.03], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
               #normal squares
               elif (i==3 or i==5) and (j%2==0 and j!=4):
-                  pass
+                 p.loadURDF('rsc/base plate/base plate white.urdf', [3-i*0.75,3-j*0.75,0], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
+                 p.loadURDF('rsc/circle/circle red.urdf', [3-i*0.75,3-j*0.75,0.03], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)              
               #normal squares
               elif (i==4 and j!=4):
-                  pass
+                 if j<4:
+                     p.loadURDF('rsc/base plate/base plate green.urdf', [3-i*0.75,3-j*0.75,0], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
+                 else:
+                     p.loadURDF('rsc/base plate/base plate blue.urdf', [3-i*0.75,3-j*0.75,0], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
+                 if (j==0 or j==8):
+                     if j==0:
+                        p.loadURDF('rsc/arrow/arrow.urdf', [3-i*0.75,3-j*0.75,0.03], p.getQuaternionFromEuler([0,0,-np.pi/2]), useFixedBase=1)
+                     else:
+                        p.loadURDF('rsc/arrow/arrow.urdf', [3-i*0.75,3-j*0.75,0.03], p.getQuaternionFromEuler([0,0,np.pi/2]), useFixedBase=1)
+                 else:
+                     p.loadURDF('rsc/square/square red.urdf', [3-i*0.75,3-j*0.75,0.03], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
               #colored paths
               elif i==0 and j==4:
-                  pass
+                 p.loadURDF('rsc/base plate/base plate cyan.urdf', [3-i*0.75,3-j*0.75,0], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
+                 p.loadURDF('rsc/arrow/arrow.urdf', [3-i*0.75,3-j*0.75,0.03], p.getQuaternionFromEuler([0,0,np.pi]), useFixedBase=1)
                #arrow
+              elif i==8 and j==4:
+                 p.loadURDF('rsc/base plate/base plate magenta.urdf', [3-i*0.75,3-j*0.75,0], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
+                 p.loadURDF('rsc/arrow/arrow.urdf', [3-i*0.75,3-j*0.75,0.03], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
+              #arrow
               elif i<4 and j==4:
-                  pass
+                 p.loadURDF('rsc/base plate/base plate cyan.urdf', [3-i*0.75,3-j*0.75,0], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
+                 p.loadURDF('rsc/triangle/triangle yellow.urdf', [3-i*0.75,3-j*0.75,0.03], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
               #colored paths
               elif i>4 and j==4:
-                  pass
+                 p.loadURDF('rsc/base plate/base plate magenta.urdf', [3-i*0.75,3-j*0.75,0], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
+                 p.loadURDF('rsc/circle/circle yellow.urdf', [3-i*0.75,3-j*0.75,0.03], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
               #colored paths
               elif j==4:
-                  pass
+                 p.loadURDF('rsc/base plate/base plate white.urdf', [3-i*0.75,3-j*0.75,0], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
+                 p.loadURDF('rsc/circle/circle red.urdf', [3-i*0.75,3-j*0.75,0.03], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
               #centre block
               else:
-                  pass
+                 p.loadURDF('rsc/base plate/base plate black.urdf', [3-i*0.75,3-j*0.75,0], p.getQuaternionFromEuler([0,0,0]), useFixedBase=1)
               #black boxes
 
+  def move(self, car, leftFrontWheel, rightFrontWheel, leftRearWheel, RighRearWheel):
+      p.setJointMotorControl2(car,  2, p.VELOCITY_CONTROL, targetVelocity=leftFrontWheel, force=15)
+      p.setJointMotorControl2(car,  3, p.VELOCITY_CONTROL, targetVelocity=rightFrontWheel, force=15)
+      p.setJointMotorControl2(car,  4, p.VELOCITY_CONTROL, targetVelocity=leftRearWheel, force=15)
+      p.setJointMotorControl2(car,  5, p.VELOCITY_CONTROL, targetVelocity=rightRearWheel, force=15)
