db79dc9a3763426c9d330c2c23de75e216c81869
Yash Sahijwani <60649571+Terabyte17@users.noreply.github.com>
2020-11-01 13:27:29 +0530

    aruco_tested. respawn corrected

---
+++ aruco_test.py
@@ -0,0 +1,36 @@
+import cv2.aruco as aruco
+import cv2
+
+# Constant parameters used in Aruco methods
+ARUCO_PARAMETERS = aruco.DetectorParameters_create()
+ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
+
+
+# Create grid board object we're using in our stream
+board = aruco.GridBoard_create(
+        markersX=2,
+        markersY=2,
+        markerLength=0.09,
+        markerSeparation=0.01,
+        dictionary=ARUCO_DICT)
+
+# Create vectors we'll be using for rotations and translations for postures
+rvecs, tvecs = None, None
+
+img=cv2.imread('testrun0.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    #Detect Aruco markers
+corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, ARUCO_DICT, parameters=ARUCO_PARAMETERS)
+
+    # Make sure all 5 markers were detected before printing them out
+if ids is not None:
+            # Print corners and ids to the console
+	for i, corner in zip(ids, corners):
+		print('ID: {}; Corners: {}'.format(i, corner))
+
+            # Outline all of the markers detected in our image
+
+img = aruco.drawDetectedMarkers(img, corners, borderColor=(0, 0, 255))
+
+cv2.imwrite("aruco_detected.png", img)
--- vision-arena/vision_arena/envs/vision2arena.py
+++ vision-arena/vision_arena/envs/vision2arena.py
@@ -34,6 +34,9 @@ class VisionArena(gym.Env):
 		p.configureDebugVisualizer(p.COV_ENABLE_WIREFRAME, 0)
 		p.configureDebugVisualizer(p.COV_ENABLE_SHADOWS, 0)
 		
+
+		self.husky = None
+
 		self.__load_arena()
 		self.respawn_car()
 
@@ -244,6 +247,7 @@ class VisionArena(gym.Env):
 			None
 		"""
 		p.removeBody(self.husky)
+		self.husky = None
 
 	def respawn_car(self):
 		"""
@@ -257,13 +261,19 @@ class VisionArena(gym.Env):
 
 			None
 		"""
-		np.random.seed(0)
+		if self.husky is not None:
+			print("Old Car being Removed")
+			p.removeBody(self.husky)
+			self.husky = None
+
 		pos = [[0,4], [4,0], [8,4], [4,8]]
 		ori = [-np.pi/2, 0, np.pi/2, np.pi]
 		x = np.random.randint(0,3)
 		self.husky = p.loadURDF('husky/husky.urdf', [4-1*pos[x][0],4-1*pos[x][1],0], p.getQuaternionFromEuler([0,0,ori[x]]))
 		self.aruco = p.loadURDF('rsc/aruco/aruco.urdf', [4-1*pos[x][0],4-1*pos[x][1],1.2], p.getQuaternionFromEuler([1.5707,0,ori[x]]))
 		p.createConstraint(self.husky, -1, self.aruco, -1, p.JOINT_FIXED, [0,0,1], [0,0,0.4], [0,0,0], childFrameOrientation = p.getQuaternionFromEuler([0,-1.5707,ori[x]]))
+		for x in range(100):
+			p.stepSimulation()
 
 	def roll_dice(self):
 		"""
